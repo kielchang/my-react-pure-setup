9cb1a670eeee7b7fd1da5d17ae695f094526cf43
kielchang <kiel.chang.w@gmail.com>
2022-06-13 23:56:17 +0800

    set up babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import {calc} from './calc'
+
 console.log('Hello Webpack Project....');
 var i=0;
 document.getElementById("btn_clickme").addEventListener("click", function(event) {
   i++;
   document.getElementById("text").innerText=`Button Click ${i} times.`
 });
+
+console.log(calc(20,5));
