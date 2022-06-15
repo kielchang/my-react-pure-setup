da127e2f59cc396345dc372153d1b9b9838b7b80
kielchang <kiel.chang.w@gmail.com>
2022-06-15 18:56:40 +0800

    setup ESLint & Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
-const path = require('path');
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, "./src/index.js"),
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ["babel-loader"],
+      },
+    ],
   },
+  plugins: [new ESLintPlugin()],
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ["*", ".js"],
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
   },
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
   },
 };
